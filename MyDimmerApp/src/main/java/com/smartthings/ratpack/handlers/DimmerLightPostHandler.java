b598ea34191d7b170c71065388a38707a06d2eb0
Darsi <SDarsi@ad.harman.com>
2017-11-03 14:29:34 -0500

    changes done to post handler

--- MyDimmerApp/src/main/java/com/smartthings/ratpack/handlers/DimmerLightPostHandler.java
+++ MyDimmerApp/src/main/java/com/smartthings/ratpack/handlers/DimmerLightPostHandler.java
@@ -1,6 +1,5 @@
 package com.smartthings.ratpack.handlers;
 
-import static ratpack.jackson.Jackson.fromJson;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,12 +14,6 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
-
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.fasterxml.jackson.databind.util.JSONWrappedObject;
-import com.smartthings.behaviour.beans.DimmerLight;
 import com.smartthings.behaviour.constants.Constants;
 
 import ratpack.exec.ExecResult;
@@ -28,7 +21,6 @@ import ratpack.exec.Promise;
 import ratpack.handling.Context;
 import ratpack.handling.Handler;
 import ratpack.http.TypedData;
-import ratpack.jackson.Jackson;
 import ratpack.test.exec.ExecHarness;
 
 public class DimmerLightPostHandler implements Handler {
@@ -43,27 +35,17 @@ public class DimmerLightPostHandler implements Handler {
 	@Override
 	public void handle(Context ctx) throws Exception {
 		System.out.println("entered post :: handle");
-		Promise<TypedData> body = ctx.getRequest().getBody();
-		ExecResult<String> result1 = ExecHarness.yieldSingle(c -> body.map(p -> p.getText()));
-		System.out.println("Level Json as String : "+result1.getValue());
-		JsonFactory _factory = new JsonFactory();
-		JsonParser parser = _factory.createJsonParser(result1.getValue().toString());
-		//JsonFactory _factory = JsonFactory.createParser(result1.getValue().toString());
-		//JSONWrappedObject json = (JSONWrappedObject) parser.parse(stringToParse);
-		Promise<DimmerLight> payLoadValue = ctx.parse(fromJson(DimmerLight.class));
-		
-		//ExecResult<String> result1 = ExecHarness.yieldSingle(c -> payLoadValue.map(p -> p.getValue()));
+		Promise<TypedData> payLoad = ctx.getRequest().getBody();
+		ExecResult<String> body = ExecHarness.yieldSingle(c -> payLoad.map(p -> p.getText()));
+		System.out.println("Level Json as String : "+body.getValue());
 		System.out.println("before post :: handle");
 		HttpClient httpclient = HttpClients.createDefault();
-		//HttpPost httppost = new HttpPost(Constants.smartAppEndpointURL + Constants.setLevlPath + result1.getValue());
 		HttpPost httppost = new HttpPost(Constants.smartAppEndpointURL + Constants.setLevlPath);
-		// + Jackson.json(result1.getValue())
 		System.out.println(httppost.getURI());
-		System.out.println(httppost.getRequestLine());
+		//System.out.println(httppost.getRequestLine());
 		httppost.setHeader("Authorization", Constants.authorizationKey);
 		httppost.setHeader("Content-Type","application/json");
-		httppost.setEntity(new StringEntity(result1.getValue()));
-		
+		httppost.setEntity(new StringEntity(body.getValue()));
 		HttpResponse response = httpclient.execute(httppost);
 		//response.getStatusLine();
 		HttpEntity entity = response.getEntity();
